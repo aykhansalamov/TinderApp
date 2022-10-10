fc9ca80c821d83157654967319561f04f0b959fc
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-10-10 20:42:14 +0400

    Show File Servlet

---
+++ src/main/java/ShowFileServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+
+// http://localhost:8080/show
+public class ShowFileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest rq, HttpServletResponse rs) throws ServletException, IOException {
+        String fileName = getClass().getClassLoader().getResource("people-list.html").getFile();
+
+        List<String> lines = Files.readAllLines(Path.of(fileName));
+        try(PrintWriter w = rs.getWriter()){
+            for(String line: lines){
+                w.println(line);
+            }
+        }
+    }
+}
