edba07ca72492a2b6e33d8738b54c5dca6576cf0
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-10-10 18:58:11 +0400

    Test Servlet

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+    // http://localhost:8080/test
+    @Override
+    protected void doGet(HttpServletRequest rq, HttpServletResponse rs) throws ServletException, IOException {
+        try (PrintWriter w = rs.getWriter()) {
+            w.println("Hello world!");
+        }
+    }
+}
